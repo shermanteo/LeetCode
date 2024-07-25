3b2c723157edc6755bd88249f7d6e65264f11b7c
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-25 22:34:00 +0800

    Update 1757. Recyclable and Low Fat Products.sql

--- SQL/1757. Recyclable and Low Fat Products.sql
+++ SQL/1757. Recyclable and Low Fat Products.sql
@@ -43,6 +43,18 @@
 -- +-------------+
 -- Explanation: Only products 1 and 3 are both low fat and recyclable.
 
+
+-- Start of SQL Question Schema
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'))
+Truncate table Products
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N')
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
+-- End of SQL Question Schema
+
+
 -- Start of Solution
 
 
