bfe79626713d386e01e9143f78cf5aca6d2f816a
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-25 22:46:32 +0800

    Create 595. Big Countries.py

---
+++ Python/Pandas/595. Big Countries.py
@@ -0,0 +1,14 @@
+
+
+
+## Start of Python Question Schema
+data = [['Afghanistan', 'Asia', 652230, 25500100, 20343000000], ['Albania', 'Europe', 28748, 2831741, 12960000000], ['Algeria', 'Africa', 2381741, 37100000, 188681000000], ['Andorra', 'Europe', 468, 78115, 3712000000], ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
+world = pd.DataFrame(data, columns=['name', 'continent', 'area', 'population', 'gdp']).astype({'name':'object', 'continent':'object', 'area':'Int64', 'population':'Int64', 'gdp':'Int64'})
+## End of Python Question Schema
+
+
+
+## Start of Solution
+
+
+## End of Solution
