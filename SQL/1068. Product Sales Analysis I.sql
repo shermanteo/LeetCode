5e258628abced162d80787940059d8329dc85684
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-26 11:04:53 +0800

    Update 1068. Product Sales Analysis I.sql

--- SQL/1068. Product Sales Analysis I.sql
+++ SQL/1068. Product Sales Analysis I.sql
@@ -57,6 +57,7 @@ Explanation:
 From sale_id = 1, we can conclude that Nokia was sold for 5000 in the year 2008.
 From sale_id = 2, we can conclude that Nokia was sold for 5000 in the year 2009.
 From sale_id = 7, we can conclude that Apple was sold for 9000 in the year 2011.
+
 */
 
 
