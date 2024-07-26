8bc7c1fdbe41cdf81090db038cf79f989748f829
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-26 12:49:04 +0800

    Update 197. Rising Temperature.sql

--- SQL/197. Rising Temperature.sql
+++ SQL/197. Rising Temperature.sql
@@ -51,11 +51,20 @@ In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 
 
 -- Start of SQL Question Schema
-
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
 -- End of SQL Question Schema
 
 
 
 -- Start of Solution
-
+SELECT current.id
+FROM Weather current
+JOIN Weather prev
+ON current.recordDate = DATE_ADD(prev.recordDate, INTERVAL 1 DAY)
+WHERE current.temperature > prev.temperature;
 -- End of Solution
