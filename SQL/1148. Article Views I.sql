92227c91aaf7c1c208f89754587a8773a4d65d9d
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-26 10:21:10 +0800

    Update 1148. Article Views I.sql

--- SQL/1148. Article Views I.sql
+++ SQL/1148. Article Views I.sql
@@ -62,9 +62,8 @@ insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4'
 
 
 -- Start of Solution
-
-
-
-
-
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC;
 -- End of Solution
