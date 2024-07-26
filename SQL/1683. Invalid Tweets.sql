0e537a80e6f158e5925a194471b2b58f4de5b8f5
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-26 10:26:25 +0800

    Create 1683. Invalid Tweets.sql

---
+++ SQL/1683. Invalid Tweets.sql
@@ -0,0 +1,58 @@
+-- Question 1683
+-- Table: Tweets
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | tweet_id       | int     |
+-- | content        | varchar |
+-- +----------------+---------+
+-- tweet_id is the primary key (column with unique values) for this table.
+-- This table contains all the tweets in a social media app.
+ 
+
+-- Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Tweets table:
+-- +----------+----------------------------------+
+-- | tweet_id | content                          |
+-- +----------+----------------------------------+
+-- | 1        | Vote for Biden                   |
+-- | 2        | Let us make America great again! |
+-- +----------+----------------------------------+
+-- Output: 
+-- +----------+
+-- | tweet_id |
+-- +----------+
+-- | 2        |
+-- +----------+
+-- Explanation: 
+-- Tweet 1 has length = 14. It is a valid tweet.
+-- Tweet 2 has length = 32. It is an invalid tweet.
+
+
+
+-- Start of SQL Question Schema
+Create table If Not Exists Tweets(tweet_id int, content varchar(50))
+Truncate table Tweets
+insert into Tweets (tweet_id, content) values ('1', 'Vote for Biden')
+insert into Tweets (tweet_id, content) values ('2', 'Let us make America great again!')
+-- End of SQL Question Schema
+
+
+
+-- Start of Solution
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
+-- End of Solution
+
