1e54dfe093cb06563ba05928a74fd27535053d4b
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-30 20:34:50 +0800

    Update 620. Not Boring Movies.sql

--- SQL/620. Not Boring Movies.sql
+++ SQL/620. Not Boring Movies.sql
@@ -65,11 +65,11 @@ insert into cinema (id, movie, description, rating) values ('5', 'House card', '
 
 -- Start of Solution
 SELECT 
- *
+  *
 FROM 
- Cinema
+  Cinema
 WHERE 
- MOD(id, 2) = 1 AND description != 'boring'
+  MOD(id, 2) = 1 AND description != 'boring'
 ORDER BY 
- rating DESC;
+  rating DESC;
 -- End of Solution
