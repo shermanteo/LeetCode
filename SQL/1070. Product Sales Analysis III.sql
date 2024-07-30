882d6cacb037e5891b9d73267f011b8983f1190b
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-30 20:47:48 +0800

    Create 1070. Product Sales Analysis III.sql

---
+++ SQL/1070. Product Sales Analysis III.sql
@@ -0,0 +1,102 @@
+/*
+
+Question 1070
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+
+Return the resulting table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+
+*/
+
+
+
+-- Start of SQL Question Schema
+Create table If Not Exists Sales (sale_id int, product_id int, year int, quantity int, price int)
+Create table If Not Exists Product (product_id int, product_name varchar(10))
+Truncate table Sales
+insert into Sales (sale_id, product_id, year, quantity, price) values ('1', '100', '2008', '10', '5000')
+insert into Sales (sale_id, product_id, year, quantity, price) values ('2', '100', '2009', '12', '5000')
+insert into Sales (sale_id, product_id, year, quantity, price) values ('7', '200', '2011', '15', '9000')
+Truncate table Product
+insert into Product (product_id, product_name) values ('100', 'Nokia')
+insert into Product (product_id, product_name) values ('200', 'Apple')
+insert into Product (product_id, product_name) values ('300', 'Samsung')
+-- End of SQL Question Schema
+
+
+
+-- Start of Solution
+SELECT
+  Sales.product_id,
+  Sales.year AS first_year,
+  Sales.quantity,
+  Sales.price
+FROM 
+  Sales 
+JOIN (
+  SELECT product_id, MIN(year) AS first_year
+  FROM Sales
+  GROUP BY product_id
+) f
+  ON Sales.product_id = f.product_id
+  AND Sales.year = f.first_year;
+-- End of Solution
