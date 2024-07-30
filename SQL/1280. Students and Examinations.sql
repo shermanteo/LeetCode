3a01297855ffde940ba08e8ae628b8c08fc18c34
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-07-30 20:29:35 +0800

    Update 1280. Students and Examinations.sql

--- SQL/1280. Students and Examinations.sql
+++ SQL/1280. Students and Examinations.sql
@@ -140,25 +140,25 @@ insert into Examinations (student_id, subject_name) values ('1', 'Math')
 
 -- Start of Solution
 SELECT 
- Students.student_id, 
- Students.student_name, 
- Subjects.subject_name, 
- COALESCE(COUNT(Examinations.student_id), 0) AS attended_exams
+  Students.student_id, 
+  Students.student_name, 
+  Subjects.subject_name, 
+  COALESCE(COUNT(Examinations.student_id), 0) AS attended_exams
 FROM 
- Students
+  Students
 CROSS JOIN 
- Subjects
+  Subjects
 LEFT JOIN 
- Examinations 
+  Examinations 
 ON 
- Students.student_id = Examinations.student_id 
+  Students.student_id = Examinations.student_id 
 AND 
- Subjects.subject_name = Examinations.subject_name
+  Subjects.subject_name = Examinations.subject_name
 GROUP BY 
- Students.student_id, 
- Students.student_name, 
- Subjects.subject_name
+  Students.student_id, 
+  Students.student_name, 
+  Subjects.subject_name
 ORDER BY 
- Students.student_id, 
- Subjects.subject_name;
+  Students.student_id, 
+  Subjects.subject_name;
 -- End of Solution
